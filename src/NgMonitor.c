31aa14a14c3aebba87a3f555619f489a8fee4a7c
heidsoft <heidsoft@sina.com>
2017-03-01 17:51:35 +0800

    update 创建gnu 风格项目

---
+++ src/NgMonitor.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : NgMonitor.c
+ Author      : heidsoft
+ Version     :
+ Copyright   : heidsoft
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
