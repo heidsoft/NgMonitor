ffd8bc2c351ef8f3846a3e9ef1b382a0de0324ca
heidsoft <heidsoft@sina.com>
2017-03-12 16:02:20 +0800

    Update 修改依赖

--- src/NgMonitor.c
+++ src/NgMonitor.c
@@ -10,10 +10,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <event.h>
 #include "utils.h"
 
 int main(void) {
 	puts("Hello World"); /* prints Hello World */
+
+	//创建一个event_base
+	struct event_base *base = event_base_new();
+	if(base != NULL){
+		printf("new event_base\n");
+	}
+
+
+
 	test1();
 	test2();
 	return EXIT_SUCCESS;
